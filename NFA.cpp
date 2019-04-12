1339f53a19253945595333fd26129ff273abab78
axelzucho <axelzucho1@gmail.com>
2019-04-12 12:12:51 -0500

    Add NFA comments

--- NFA.cpp
+++ NFA.cpp
@@ -16,17 +16,24 @@ NFA::NFA(string filepath) {
         opened_file = false;
         return;
     }
+    // We could open the file.
     opened_file = true;
 
+    // The initial node by definition is always 0.
     this->initial_node = 0;
 
+    // Get the amount of states.
     file >> this->state_amount;
+    // Get the alphabet size.
     file >> this->alphabet_size;
 
     int final_state_amount;
 
+    // Gets the amount of final states.
     file >> final_state_amount;
+    // Reserves this amount in the vector.
     this->final_states.reserve((unsigned long)final_state_amount);
+    // Iterates for each of the final states and adds them to the vector.
     for(int i = 0; i < final_state_amount; ++i){
         int state;
         file >> state;
@@ -36,14 +43,19 @@ NFA::NFA(string filepath) {
     int transition_amount;
 
     file >> transition_amount;
+    // Iterates for each transition in the NFA.
     for(int i = 0; i < transition_amount; ++i){
         int initial_node, final_node;
         char symbol;
+        // Gets the transition data from the file.
         file >> initial_node >> final_node >> symbol;
+        // Create a pair that will function as the key in our map.
         pair<int, char> key = make_pair(initial_node, symbol);
+        // If there are already existing transitions for the given key, just add the new destination to the vector.
         if (this->transitions.find(key) != transitions.end()){
             transitions[key].push_back(final_node);
         } else {
+            // If not, add a new vector containing the destination state.
             this->transitions[key] = {final_node};
         }
     }
--- NFA.h
+++ NFA.h
@@ -12,16 +12,29 @@ using namespace std;
 
 class NFA {
 private:
+    // Shows if the input file could be opened.
     bool opened_file;
+
+    // The amount of states in the NFA
     int state_amount;
+
+    // The alphabet size for the NFA.
     int alphabet_size;
+
+    // The initial node (by definition always 0).
     int initial_node;
+
+    // Contains all final states in the DFA.
     vector<int> final_states;
+
+    // Dictionary that maps a state and a symbol to another state.
     map<pair<int,char>, vector<int>> transitions;
 
+    // So that DFA can access private values.
     friend class DFA;
 public:
     explicit NFA(string filepath);
+    // Returns if the file could be found.
     bool found_file();
 
 };
